ff3ff2ff8d01b373849c6da1c50281f120900245
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-07-21 19:00:06 +0530

    feat: add PalindromeCheck.java (07-21-2026)

---
+++ PalindromeCheck.java
@@ -0,0 +1,9 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(""madam"")); // true
+    }
+}
