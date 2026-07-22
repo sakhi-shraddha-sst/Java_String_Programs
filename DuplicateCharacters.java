5e73c6e336c5a3dca06ed0287d229d1fd5185604
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-07-22 10:00:07 +0530

    feat: add DuplicateCharacters.java (07-22-2026)

---
+++ DuplicateCharacters.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DuplicateCharacters {
+    public static void findDuplicates(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        map.forEach((k, v) -> {
+            if (v > 1) System.out.println(k + "": "" + v);
+        });
+    }
+
+    public static void main(String[] args) {
+        findDuplicates(""programming"");
+    }
+}
