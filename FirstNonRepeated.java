4869c8ce243034e34da2d73286934253fdd08f1c
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-07-23 10:00:06 +0530

    feat: add FirstNonRepeated.java (07-23-2026)

---
+++ FirstNonRepeated.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeated {
+    public static char firstNonRepeated(String str) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1)
+                return entry.getKey();
+        }
+        return '_';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstNonRepeated("swiss")); // w
+    }
+}
