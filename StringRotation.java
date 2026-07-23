1800de40189f9cb03602a57c4d2f0a6149b8a213
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-07-23 20:00:06 +0530

    feat: add StringRotation.java (07-23-2026)

---
+++ StringRotation.java
@@ -0,0 +1,10 @@
+public class StringRotation {
+
+    public static boolean isRotation(String s1, String s2) {
+        return (s1.length() == s2.length()) && (s1 + s1).contains(s2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isRotation("abcd", "cdab")); // true
+    }
+}
